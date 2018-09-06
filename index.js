2f7a024ae84c96c49ae789d0c151c00ada4ca68e
Glitch (choo-for-bee) <none>
2018-09-06 23:52:39 +0000

    👀🚟 Checkpoint ./index.js:875713/562

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 // import choo
-var choo = require('choo')
+const choo = require('choo')
+
+// 'html' is a template helper for choo
+const html = require('choo/html')
+
 
 // initialize choo
-var app = choo()
+const app = choo()
 
-// start app
+
+// creating a template (think functional component)
+const main = function() {
+  return html`<div>choo animals</div>`
+}
+
+// creating a route (this is an api method)
+app.route('/', main)
+
+// start app (?) (also an api method)
 app.mount('div')
-
