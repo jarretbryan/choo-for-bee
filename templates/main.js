5732270a3335700d5d96bf9702fab437fbbe8fe7
Glitch (choo-for-bee) <none>
2018-09-07 00:44:38 +0000

    🌦📡 Checkpoint ./index.js:875713/48 ./templates/main.js:875713/2092

--- templates/main.js
+++ templates/main.js
@@ -4,18 +4,19 @@ const html = require('choo/html')
 // export module
 // use function declaration here, not an arrow function
 // the function takes in state from the parent template
-module.exports =  function(state){
-  let type = state.animals.type
-  let x = state.animals.x
-  let y = state.animals.y
-  
-  
+module.exports = function (state) {
+  var type = state.animals.type
+  var x = state.animals.x
+  var y = state.animals.y
+  // note - i wanted to use let above, but i kept getting an error, so var instead
+
+  // create html template
   return html`
-    <div class='container'>
-      <div class='grass'>
-        <img src='/assets/bg.gif'/>
-        <img src='/assets/${type}.gif' style='left: ${x}px; top:${y}px;'
+    <div class="container">
+      <div class="grass">
+        <img src="/assets/bg.gif" />
+        <img src="/assets/${type}.gif" style="left: ${x}px; top: ${y}px;" />
       </div>
     </div>
   `
-}  
+}
