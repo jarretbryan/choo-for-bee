6fe2d093d04b007ac8da9a437c80f5083cf3bc12
Glitch (choo-for-bee) <none>
2018-09-07 00:13:49 +0000

    🏁🏉 Checkpoint ./templates/main.js:875713/313 ./index.js:875713/190

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ const choo = require('choo')
 // 'html' is a template helper for choo
 const html = require('choo/html')
 
+// like in react for components
+// choo requires templates to be imported 
+// or since it's plain js - required
+const main = require('./templates/main.js')
+
 
 // initialize choo
 const app = choo()
 
 
 // creating a template (think functional component)
-const main = function() {
-  return html`<div>choo aminals</div>`
-}
+// const main = function() {
+//   return html`<div>choo aminals</div>`
+// }
 
 // creating a route (this is an api method)
 // app.route takes in two functions: the path, and then the template
--- templates/main.js
+++ templates/main.js
@@ -2,4 +2,13 @@
 const html = require('choo/html')
 
 // export module
-module.exports = ()
+// use function declaration here, not an arrow function
+module.exports =  function(){
+  return html`
+    <div class='container'>
+      <div class='grass'>
+        <img src='/assets/bg.gif'/>
+      </div>
+    </div>
+  `
+}  
